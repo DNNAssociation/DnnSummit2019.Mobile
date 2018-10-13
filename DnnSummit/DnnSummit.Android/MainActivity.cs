8c4d35eab0520889108b939d0f6da3c4cb3e9f83
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-10-13 19:30:46 -0400

    Updated app name from DnnSummit -> Dnn Summit

--- DnnSummit/DnnSummit.Android/MainActivity.cs
+++ DnnSummit/DnnSummit.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace DnnSummit.Droid
 {
-    [Activity(Label = "DnnSummit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Dnn Summit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

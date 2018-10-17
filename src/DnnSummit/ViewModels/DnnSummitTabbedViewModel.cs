3de6f9d6e02c7068b7d09acea0c7ba13f6b14ed1
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-10-17 00:20:50 -0400

    Added info button to tabbed view and wired up button

---
+++ src/DnnSummit/ViewModels/DnnSummitTabbedViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services;
+using System.Windows.Input;
+
+namespace DnnSummit.ViewModels
+{
+    public class DnnSummitTabbedViewModel : BindableBase
+    {
+        protected IPageDialogService PageDialogService { get; }
+        public DnnSummitTabbedViewModel(IPageDialogService pageDialogService)
+        {
+            PageDialogService = pageDialogService;
+
+            Info = new DelegateCommand(OnInfo);
+        }
+
+        
+        public ICommand Info { get; }
+
+        private async void OnInfo()
+        {
+            await PageDialogService.DisplayAlertAsync("Not Implemented", "This feature is not implemented yet", "OK");
+        }
+    }
+}

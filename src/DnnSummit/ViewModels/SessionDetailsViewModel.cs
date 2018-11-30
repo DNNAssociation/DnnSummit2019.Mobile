3d2c9c3ca13aed4fd899dfe3f106f6e4d13cc40e
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-11-29 21:48:18 -0500

    Added styling to session details page

--- src/DnnSummit/ViewModels/SessionDetailsViewModel.cs
+++ src/DnnSummit/ViewModels/SessionDetailsViewModel.cs
@@ -28,6 +28,72 @@ namespace DnnSummit.ViewModels
             }
         }
 
+        private string _image;
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                SetProperty(ref _image, value);
+                RaisePropertyChanged(nameof(Image));
+            }
+        }
+
+        private string _room;
+        public string Room
+        {
+            get { return _room; }
+            set
+            {
+                SetProperty(ref _room, value);
+                RaisePropertyChanged(nameof(Room));
+            }
+        }
+
+        private string _fullName;
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                SetProperty(ref _fullName, value);
+                RaisePropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _session;
+        public string Session
+        {
+            get { return _session; }
+            set
+            {
+                SetProperty(ref _session, value);
+                RaisePropertyChanged(nameof(Session));
+            }
+        }
+
+        private string _timeSlot;
+        public string TimeSlot
+        {
+            get { return _timeSlot; }
+            set
+            {
+                SetProperty(ref _timeSlot, value);
+                RaisePropertyChanged(nameof(TimeSlot));
+            }
+        }
+
+        private SessionTrack _sessionTrack;
+        public SessionTrack SessionTrack
+        {
+            get { return _sessionTrack; }
+            set
+            {
+                SetProperty(ref _sessionTrack, value);
+                RaisePropertyChanged(nameof(SessionTrack));
+            }
+        }
+
         public void OnNavigatingTo(NavigationParameters parameters)
         {
             var isSuccessful = false;
@@ -39,6 +105,12 @@ namespace DnnSummit.ViewModels
                 {
                     Title = session.Title;
                     Description = session.Description;
+                    Image = session.Speaker.HeadshotImage;
+                    Room = session.Room;
+                    FullName = session.Speaker.Name;
+                    Session = "Session 1";
+                    TimeSlot = "9:10-10:10";
+                    SessionTrack = session.Track;
 
                     isSuccessful = true;
                 }

b2da92e988a926db2ca786fa573b1b1ba9b2903d
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-11 22:10:53 -0500

    Refactored title view to be a shared view. added sponsor page and view model

---
+++ src/DnnSummit/Controls/LogoTitleView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DnnSummit.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LogoTitleView : ContentView
+	{
+		public LogoTitleView ()
+		{
+			InitializeComponent ();
+		}
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            nameof(Title),
+            typeof(string),
+            typeof(LogoTitleView),
+            null,
+            propertyChanged: OnTitlePropertyChanged);
+
+        private static void OnTitlePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var context = (LogoTitleView)bindable;
+            if (context != null)
+            {
+                context.TitleLabel.Text = (string)newValue;
+            }
+        }
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create(
+            nameof(Source),
+            typeof(ImageSource),
+            typeof(LogoTitleView),
+            null,
+            propertyChanged: OnSourcePropertyChanged);
+
+        private static void OnSourcePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var context = (LogoTitleView)bindable;
+            if (context != null)
+            {
+                context.IconImage.Source = (ImageSource)newValue;
+            }
+        }
+    }
+}
---
+++ src/DnnSummit/ViewModels/SponsorsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Mvvm;
+
+namespace DnnSummit.ViewModels
+{
+    public class SponsorsViewModel : BindableBase
+    {
+    }
+}

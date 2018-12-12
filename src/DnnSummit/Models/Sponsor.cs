d29389fb3c418bd401ff6d4065f6d0af0b8ae2db
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-12 11:28:14 -0500

    wired up sponsors service to the sponsors view model and page. It is now properly loading the data onto the page

--- src/DnnSummit/Models/Sponsor.cs
+++ src/DnnSummit/Models/Sponsor.cs
@@ -2,7 +2,7 @@
 {
     public class Sponsor
     {
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string ImageLink { get; set; }
         public string Homepage { get; set; }
     }
--- src/DnnSummit/ViewModels/SponsorsViewModel.cs
+++ src/DnnSummit/ViewModels/SponsorsViewModel.cs
@@ -1,16 +1,72 @@
-﻿using DnnSummit.Models;
+﻿using DnnSummit.Data.Services.Interfaces;
+using DnnSummit.Models;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System.Collections.ObjectModel;
 
 namespace DnnSummit.ViewModels
 {
-    public class SponsorsViewModel : BindableBase
+    public class SponsorsViewModel : BindableBase, INavigatingAware
     {
-        public ObservableCollection<Sponsor> Sponsors { get; }
+        protected ISponsorService SponsorService { get; }
+        public ObservableCollection<Sponsor> Titles { get; }
+        public ObservableCollection<Sponsor> Platinums { get; }
+        public ObservableCollection<Sponsor> Golds { get; }
+        public ObservableCollection<Sponsor> Silvers { get; }
+        public ObservableCollection<Sponsor> Bronzes { get; }
 
-        public SponsorsViewModel()
+        public SponsorsViewModel(ISponsorService sponsorService)
         {
-            Sponsors = new ObservableCollection<Sponsor>();
+            SponsorService = sponsorService;
+            Titles = new ObservableCollection<Sponsor>();
+            Platinums = new ObservableCollection<Sponsor>();
+            Golds = new ObservableCollection<Sponsor>();
+            Silvers = new ObservableCollection<Sponsor>();
+            Bronzes = new ObservableCollection<Sponsor>();
+        }
+
+        private void ClearData()
+        {
+            Titles.Clear();
+            Platinums.Clear();
+            Golds.Clear();
+            Silvers.Clear();
+            Bronzes.Clear();
+        }
+
+        public async void OnNavigatingTo(INavigationParameters parameters)
+        {
+            var data = await SponsorService.GetAsync();
+            ClearData();
+
+            foreach (var item in data)
+            {
+                var sponsor = new Sponsor
+                {
+                    Name = item.Name,
+                    Homepage = item.Homepage,
+                    ImageLink = item.ImageLink
+                };
+
+                switch (item.Level)
+                {
+                    case Data.Models.SponsorLevel.Title:
+                        Titles.Add(sponsor);
+                        break;
+                    case Data.Models.SponsorLevel.Platinum:
+                        Platinums.Add(sponsor);
+                        break;
+                    case Data.Models.SponsorLevel.Gold:
+                        Golds.Add(sponsor);
+                        break;
+                    case Data.Models.SponsorLevel.Silver:
+                        Silvers.Add(sponsor);
+                        break;
+                    case Data.Models.SponsorLevel.Bronze:
+                        Bronzes.Add(sponsor);
+                        break;
+                }
+            }
         }
     }
 }

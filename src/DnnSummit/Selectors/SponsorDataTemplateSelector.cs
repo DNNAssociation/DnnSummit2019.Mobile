6aee61dd06317588a3687168a98a53b10b2d98e3
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-12 14:56:55 -0500

    current work for building sponsors page

---
+++ src/DnnSummit/Selectors/SponsorDataTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using DnnSummit.Models;
+using System;
+using Xamarin.Forms;
+
+namespace DnnSummit.Selectors
+{
+    public class SponsorDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TitleTemplate { get; set; }
+        public DataTemplate PlatinumTemplate { get; set; }
+        public DataTemplate GoldTemplate { get; set; }
+        public DataTemplate SilverTemplate { get; set; }
+        public DataTemplate BronzeTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var sponsor = (ISponsorGroup)item;
+            if (sponsor != null)
+            {
+                switch (sponsor.Level)
+                {
+                    case SponsorType.Title:
+                        return TitleTemplate;
+                    case SponsorType.Platinum:
+                        return PlatinumTemplate; 
+                    case SponsorType.Gold:
+                        return GoldTemplate;
+                    case SponsorType.Silver:
+                        return SilverTemplate;
+                    case SponsorType.Bronze:
+                        return BronzeTemplate;
+                }
+            }
+
+            throw new NotSupportedException("Unable to find the correct template");
+        }
+    }
+}
--- src/DnnSummit/ViewModels/SponsorsViewModel.cs
+++ src/DnnSummit/ViewModels/SponsorsViewModel.cs
@@ -10,31 +10,9 @@ namespace DnnSummit.ViewModels
     public class SponsorsViewModel : BindableBase, INavigatingAware
     {
         protected ISponsorService SponsorService { get; }
-
-        private ObservableCollection<Sponsor> _titles;
-        public ObservableCollection<Sponsor> Titles
-        {
-            get { return _titles; }
-            set
-            {
-                SetProperty(ref _titles, value);
-                RaisePropertyChanged(nameof(Titles));
-                RaisePropertyChanged(nameof(HasTitles));
-            }
-        }
-
-        private ObservableCollection<Sponsor> _platinums;
-        public ObservableCollection<Sponsor> Platinums
-        {
-            get { return _platinums; }
-            set
-            {
-                SetProperty(ref _platinums, value);
-                RaisePropertyChanged(nameof(Platinums));
-                RaisePropertyChanged(nameof(HasPlaltinums));
-            }
-        }
-
+        public ObservableCollection<object> Sponsors { get; }
+        public ObservableCollection<Sponsor> Titles { get; }
+        public ObservableCollection<Sponsor> Platinums { get; }
         public ObservableCollection<Sponsor> Golds { get; }
         public ObservableCollection<Sponsor> Silvers { get; }
         public ObservableCollection<Sponsor> Bronzes { get; }
@@ -67,6 +45,7 @@ namespace DnnSummit.ViewModels
         public SponsorsViewModel(ISponsorService sponsorService)
         {
             SponsorService = sponsorService;
+            Sponsors = new ObservableCollection<object>();
             Titles = new ObservableCollection<Sponsor>();
             Platinums = new ObservableCollection<Sponsor>();
             Golds = new ObservableCollection<Sponsor>();
@@ -82,77 +61,64 @@ namespace DnnSummit.ViewModels
 
         private void ClearData()
         {
+            Sponsors.Clear();
             Titles.Clear();
             Platinums.Clear();
             Golds.Clear();
             Silvers.Clear();
             Bronzes.Clear();
-            
         }
 
         public async void OnNavigatingTo(INavigationParameters parameters)
         {
             var data = await SponsorService.GetAsync();
-            var sponsors = data.Select(x => new Sponsor
+            ClearData();
+
+            foreach (var item in data)
             {
-                Name = x.Name,
-                Homepage = x.Homepage,
-                ImageLink = x.ImageLink
-            });
-
-            Titles = new ObservableCollection<Sponsor>(data
-                .Where(x => x.Level == Data.Models.SponsorLevel.Title)
-                .Select(x => new Sponsor
+                var sponsor = new Sponsor
                 {
-                    Name = x.Name,
-                    Homepage = x.Homepage,
-                    ImageLink = x.ImageLink
-                }));
+                    Name = item.Name,
+                    Homepage = item.Homepage,
+                    ImageLink = item.ImageLink,
+                    Level = (SponsorType)item.Level
+                };
 
-            Platinums = new ObservableCollection<Sponsor>(data
-                .Where(x => x.Level == Data.Models.SponsorLevel.Platinum)
-                .Select(x => new Sponsor
+
+                switch (item.Level)
                 {
-                    Name = x.Name,
-                    Homepage = x.Homepage,
-                    ImageLink = x.ImageLink
-                }));
-
-            //ClearData();
-
-            //foreach (var item in data)
-            //{
-            //    var sponsor = new Sponsor
-            //    {
-            //        Name = item.Name,
-            //        Homepage = item.Homepage,
-            //        ImageLink = item.ImageLink
-            //    };
-
-            //    switch (item.Level)
-            //    {
-            //        case Data.Models.SponsorLevel.Title:
-            //            Titles.Add(sponsor);
-            //            RaisePropertyChanged(nameof(HasTitles));
-            //            break;
-            //        case Data.Models.SponsorLevel.Platinum:
-            //            Platinums.Add(sponsor);
-            //            RaisePropertyChanged(nameof(HasPlaltinums));
-            //            break;
-            //        case Data.Models.SponsorLevel.Gold:
-            //            Golds.Add(sponsor);
-            //            RaisePropertyChanged(nameof(HasGolds));
-            //            break;
-            //        case Data.Models.SponsorLevel.Silver:
-            //            Silvers.Add(sponsor);
-            //            RaisePropertyChanged(nameof(HasSilvers));
-            //            break;
-            //        case Data.Models.SponsorLevel.Bronze:
-            //            Bronzes.Add(sponsor);
-            //            RaisePropertyChanged(nameof(HasBronzes));
-            //            break;
-            //    }
-            //}
+                    case Data.Models.SponsorLevel.Title:
+                        Titles.Add(sponsor);
+                        Sponsors.Add(new TitlePlatinumSponsor
+                        {
+                            DataContext = sponsor,
+                            Level = SponsorType.Title
+                        });
+                        RaisePropertyChanged(nameof(HasTitles));
+                        break;
+                    case Data.Models.SponsorLevel.Platinum:
+                        Platinums.Add(sponsor);
+                        Sponsors.Add(new TitlePlatinumSponsor
+                        {
+                            DataContext = sponsor,
+                            Level = SponsorType.Platinum
+                        });
+                        RaisePropertyChanged(nameof(HasPlaltinums));
+                        break;
+                    case Data.Models.SponsorLevel.Gold:
+                        Golds.Add(sponsor);
+                        RaisePropertyChanged(nameof(HasGolds));
+                        break;
+                    case Data.Models.SponsorLevel.Silver:
+                        Silvers.Add(sponsor);
+                        RaisePropertyChanged(nameof(HasSilvers));
+                        break;
+                    case Data.Models.SponsorLevel.Bronze:
+                        Bronzes.Add(sponsor);
+                        RaisePropertyChanged(nameof(HasBronzes));
+                        break;
+                }
+            }
         }
     }
 }

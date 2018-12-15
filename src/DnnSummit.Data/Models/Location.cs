b8e4431e544ae284f200eb1030cdc3e1476674d8
David Poindexter <dpoindexter@nvisionative.com>
2018-12-15 16:36:06 -0500

    Implement Locations support within DnnSummit.Data

---
+++ src/DnnSummit.Data/Models/Location.cs
@@ -0,0 +1,34 @@
+﻿namespace DnnSummit.Data.Models
+{
+    public class Location
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string HotelName { get; set; }
+        public string HotelStandardsTitle { get; set; }
+        public string HotelStandard1 { get; set; }
+        public string HotelStandard2 { get; set; }
+        public string HotelStandard3 { get; set; }
+        public string HotelStandard4 { get; set; }
+        public string HotelStandard5 { get; set; }
+        public string HotelStandard6 { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DirectionsFromAirport { get; set; }
+        public string ParkingInformation { get; set; }
+        public string LocalAttractionsTitle { get; set; }
+        public string LocalAttraction1 { get; set; }
+        public string LocalAttraction2 { get; set; }
+        public string LocalAttraction3 { get; set; }
+        public string LocalAttraction4 { get; set; }
+        public string LocalAttraction5 { get; set; }
+        public string LocalAttraction6 { get; set; }
+        public string Image { get; set; }
+        public string LearnMoreUrl { get; set; }
+        public string BookNowUrl { get; set; }
+        public string PhoneNumberUrl { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string MapUrl { get; set; }
+        public string WebsiteName { get; set; }
+    }
+}
---
+++ src/DnnSummit.Data/Services/LocationService.cs
@@ -0,0 +1,53 @@
+﻿using DnnSummit.Data.Extensions;
+using DnnSummit.Data.Models;
+using DnnSummit.Data.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnnSummit.Data.Services
+{
+    internal class LocationService : BaseService<TwoSexyContent.Location>, ILocationService
+    {
+        public LocationService()
+            : base("https://www.dnnsummit.org/desktopmodules/2sxc/api/app/DnnSummit2019/Query/", "GetLocations")
+        {
+        }
+
+        public async Task<IEnumerable<Location>> GetAsync()
+        {
+            var locations = await QueryAsync();
+            return locations.Select(x => new Location
+            {
+                Title = x.Title,
+                Description = x.Description,
+                HotelName = x.HotelName,
+                HotelStandardsTitle = x.HotelStandardsTitle,
+                HotelStandard1 = x.HotelStandard1,
+                HotelStandard2 = x.HotelStandard2,
+                HotelStandard3 = x.HotelStandard3,
+                HotelStandard4 = x.HotelStandard4,
+                HotelStandard5 = x.HotelStandard5,
+                HotelStandard6 = x.HotelStandard6,
+                Address = x.Address,
+                PhoneNumber = x.PhoneNumber,
+                DirectionsFromAirport = x.DirectionsFromAirport,
+                ParkingInformation = x.ParkingInformation,
+                LocalAttractionsTitle = x.LocalAttractionsTitle,
+                LocalAttraction1 = x.LocalAttraction1,
+                LocalAttraction2 = x.LocalAttraction2,
+                LocalAttraction3 = x.LocalAttraction3,
+                LocalAttraction4 = x.LocalAttraction4,
+                LocalAttraction5 = x.LocalAttraction5,
+                LocalAttraction6 = x.LocalAttraction6,
+                Image = $"https://www.dnnsummit.org{x.Image}",
+                LearnMoreUrl = x.LearnMoreUrl,
+                BookNowUrl = x.BookNowUrl,
+                PhoneNumberUrl = x.PhoneNumberUrl,
+                WebsiteUrl = x.WebsiteUrl,
+                MapUrl = x.MapUrl,
+                WebsiteName = x.WebsiteName
+            });
+        }
+    }
+}

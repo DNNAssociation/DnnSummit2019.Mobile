946a778b6db90453fe456a0414ec0ac27ede506a
David Poindexter <dpoindexter@nvisionative.com>
2018-12-15 17:10:05 -0500

    Add IEnumerables for HotelStandards and LocalAttractions

--- src/DnnSummit.Data/Models/Location.cs
+++ src/DnnSummit.Data/Models/Location.cs
@@ -1,4 +1,6 @@
-﻿namespace DnnSummit.Data.Models
+﻿using System.Collections.Generic;
+
+namespace DnnSummit.Data.Models
 {
     public class Location
     {
@@ -6,23 +8,13 @@
         public string Description { get; set; }
         public string HotelName { get; set; }
         public string HotelStandardsTitle { get; set; }
-        public string HotelStandard1 { get; set; }
-        public string HotelStandard2 { get; set; }
-        public string HotelStandard3 { get; set; }
-        public string HotelStandard4 { get; set; }
-        public string HotelStandard5 { get; set; }
-        public string HotelStandard6 { get; set; }
+        public IEnumerable<string> HotelStandards { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string DirectionsFromAirport { get; set; }
         public string ParkingInformation { get; set; }
         public string LocalAttractionsTitle { get; set; }
-        public string LocalAttraction1 { get; set; }
-        public string LocalAttraction2 { get; set; }
-        public string LocalAttraction3 { get; set; }
-        public string LocalAttraction4 { get; set; }
-        public string LocalAttraction5 { get; set; }
-        public string LocalAttraction6 { get; set; }
+        public IEnumerable<string> LocalAttractions { get; set; }
         public string Image { get; set; }
         public string LearnMoreUrl { get; set; }
         public string BookNowUrl { get; set; }

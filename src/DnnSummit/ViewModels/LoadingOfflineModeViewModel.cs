37a9ff4e702330b4b4d72a0bc8b1c131d8473c27
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-22 00:29:28 -0500

    Added ViewModel and wired up the view/viewmodel

---
+++ src/DnnSummit/ViewModels/LoadingOfflineModeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+
+namespace DnnSummit.ViewModels
+{
+    public class LoadingOfflineModeViewModel : BindableBase, INavigatingAware
+    {
+        private const string OfflineMessage_Pattern = "Unable to update content, information current as of {0}, Contact event staff for any change or retry with internet.";
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                SetProperty(ref _message, value);
+                RaisePropertyChanged(nameof(Message));
+            }
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+            var date = DateTime.Now.ToString("MM/dd/yyyy");
+            Message = string.Format(OfflineMessage_Pattern, date);
+        }
+    }
+}

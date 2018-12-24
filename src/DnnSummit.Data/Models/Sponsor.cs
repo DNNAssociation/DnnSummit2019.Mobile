53d93ba3db0ffee1464e93a1ff9119f61b2a3b38
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-12-24 00:26:53 -0500

    Updated sponsors to use offline images

--- src/DnnSummit.Data/Models/Sponsor.cs
+++ src/DnnSummit.Data/Models/Sponsor.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
         public string Homepage { get; set; }
-        public string ImageLink { get; set; }
+        public byte[] Image { get; set; }
         public SponsorLevel Level { get; set; }
     }
 }

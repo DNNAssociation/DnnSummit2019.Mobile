9da73ad4d137867484679718885295683b19e64d
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-01-28 15:22:46 -0500

    created non sticky list view renderer which is iOS specific so it matches functionality on android

---
+++ src/DnnSummit.iOS/Renderers/NoStickyListViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CoreGraphics;
+using DnnSummit.Controls;
+using DnnSummit.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NoStickyListView), typeof(NoStickyListViewRenderer))]
+namespace DnnSummit.iOS.Renderers
+{
+    public class NoStickyListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TableHeaderView = new UIView(new CGRect(0, 0, Control.TableHeaderView.Bounds.Width, 140));
+                Control.ContentInset = new UIEdgeInsets(-50, 0, 0, 0);
+                SetNativeControl(Control);
+            }
+        }
+    }
+}

7eb56fdd07496e121bdd79f485496a5c8a0e4934
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-01-30 12:03:59 -0500

    Updated assets for iOS for power and event_note

--- src/DnnSummit/Models/InfoType.cs
+++ src/DnnSummit/Models/InfoType.cs
@@ -4,8 +4,7 @@ namespace DnnSummit.Models
 {
     public enum InfoType
     {
-        // todo - update this to "power" from material icons
-        [ImageResource(Name = "business")]
+        [ImageResource(Name = "power")]
         Sponsors,
 
         [ImageResource(Name = "business")]

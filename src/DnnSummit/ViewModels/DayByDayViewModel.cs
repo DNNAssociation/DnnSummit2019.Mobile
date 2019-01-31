2db29ab9325f9ed48fbea15f106c879aa91206c9
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-01-31 18:32:59 -0500

    Added retry logic to handle data pull failures

--- src/DnnSummit/ViewModels/DayByDayViewModel.cs
+++ src/DnnSummit/ViewModels/DayByDayViewModel.cs
@@ -1,29 +1,40 @@
 ﻿using DnnSummit.Data.Services.Interfaces;
 using DnnSummit.Extensions;
+using DnnSummit.Manager.Interfaces;
 using DnnSummit.Models;
+using DnnSummit.ViewModels.Interfaces;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DnnSummit.ViewModels
 {
-    public class DayByDayViewModel : BindableBase, INavigatingAware
+    public class DayByDayViewModel : BindableBase, INavigatingAware, IHasDataRetrieval
     {
         protected INavigationService NavigationService { get; }
         protected IScheduleService ScheduleService { get; }
+        protected IPageDialogService PageDialogService { get; }
+        protected IErrorRetryManager ErrorRetryManager { get; }
         public string Title => "Day-By-Day";
         public ObservableCollection<Event> Days { get; set; }
         public ICommand DaySelected { get; }        
 
         public DayByDayViewModel(
             INavigationService navigationService,
-            IScheduleService scheduleService)
+            IScheduleService scheduleService,
+            IPageDialogService pageDialogService,
+            IErrorRetryManager errorRetryManager)
         {
             NavigationService = navigationService;
             ScheduleService = scheduleService;
+            PageDialogService = pageDialogService;
+            ErrorRetryManager = errorRetryManager;
 
             DaySelected = new DelegateCommand<Event>(OnDaySelected);
             Days = new ObservableCollection<Event>();
@@ -41,33 +52,45 @@ namespace DnnSummit.ViewModels
             }
         }
 
+        public async Task OnLoadAsync(INavigationParameters parameters, int attempt = 0)
+        {
+            try
+            {
+                var days = await ScheduleService.GetAsync();
+
+                Days.Clear();
+                foreach (var item in days)
+                {
+                    Days.Add(new Event
+                    {
+                        Title = item.Title,
+                        Notes = item.CardDescription,
+                        Description = item.CardDescription,
+                        Avatar = item.Title.ToScheduleType(),
+                        Banner = (item.BannerTitle, item.BannerHeading, item.BannerImage),
+                        Retrieved = item.Retrieved,
+                        ContentSections = item.Sections.Select(x => new ScheduleContent
+                        {
+                            Title = x.Title,
+                            SubTitle = x.SubTitle,
+                            SubTitleNormal = x.SubTitleNormal,
+                            Heading = x.Heading,
+                            Description = x.Description,
+                            VideoLink = x.VideoLink,
+                            VideoButtonTitle = x.VideoButtonTitle
+                        })
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                await ErrorRetryManager.HandleRetryAsync(this, parameters, attempt);
+            }
+        }
+
         public async void OnNavigatingTo(INavigationParameters parameters)
         {
-            var days = await ScheduleService.GetAsync();
-
-            Days.Clear();
-            foreach (var item in days)
-            {
-                Days.Add(new Event
-                {
-                    Title = item.Title,
-                    Notes = item.CardDescription,
-                    Description = item.CardDescription,
-                    Avatar = item.Title.ToScheduleType(),
-                    Banner = (item.BannerTitle, item.BannerHeading, item.BannerImage),
-                    Retrieved = item.Retrieved,
-                    ContentSections = item.Sections.Select(x => new ScheduleContent
-                    {
-                        Title = x.Title,
-                        SubTitle = x.SubTitle,
-                        SubTitleNormal = x.SubTitleNormal,
-                        Heading = x.Heading,
-                        Description = x.Description,
-                        VideoLink = x.VideoLink,
-                        VideoButtonTitle = x.VideoButtonTitle
-                    })
-                });
-            }
+            await OnLoadAsync(parameters);
         }
     }
 }
--- src/DnnSummit/ViewModels/VenueViewModel.cs
+++ src/DnnSummit/ViewModels/VenueViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using DnnSummit.Data.Services.Interfaces;
+using DnnSummit.Manager.Interfaces;
 using DnnSummit.Models;
+using DnnSummit.ViewModels.Interfaces;
 using Prism.Mvvm;
 using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace DnnSummit.ViewModels
 {
-    public class VenueViewModel : BindableBase, INavigatingAware
+    public class VenueViewModel : BindableBase, INavigatingAware, IHasDataRetrieval
     {
         protected ILocationService LocationService { get; }
+        protected IErrorRetryManager ErrorRetryManager { get; }
 
         public string Title => "Location";
 
@@ -28,42 +33,57 @@ namespace DnnSummit.ViewModels
             }
         }
 
-        public VenueViewModel(ILocationService locationService)
+        public VenueViewModel(
+            ILocationService locationService,
+            IErrorRetryManager errorRetryManager)
         {
             LocationService = locationService;
+            ErrorRetryManager = errorRetryManager;
             Locations = new ObservableCollection<Location>();
             Pages = new ObservableCollection<Location>();
         }
 
         public async void OnNavigatingTo(INavigationParameters parameters)
         {
-            var data = await LocationService.GetAsync();
-            Locations.Clear();
-            foreach (var item in data)
+            await OnLoadAsync(parameters);
+        }
+
+        public async Task OnLoadAsync(INavigationParameters parameters, int attempt = 0)
+        {
+            try
             {
-                Locations.Add(new Location
+                var data = await LocationService.GetAsync();
+                Locations.Clear();
+                foreach (var item in data)
                 {
-                    Title = item.Title,
-                    Description = item.Description,
-                    HotelName = item.HotelName,
-                    HotelStandardsTitle = item.HotelStandardsTitle,
-                    HotelStandards = item.HotelStandards,
-                    Address = item.Address,
-                    PhoneNumber = item.PhoneNumber,
-                    DirectionsFromAirport = item.DirectionsFromAirport,
-                    ParkingInformation = item.ParkingInformation,
-                    LocalAttractionsTitle = item.LocalAttractionsTitle,
-                    LocalAttractions = item.LocalAttractions,
-                    Image = ImageSource.FromStream(() => new MemoryStream(item.Image)),
-                    LearnMoreUrl = item.LearnMoreUrl,
-                    BookNowUrl = item.BookNowUrl,
-                    PhoneNumberUrl = item.PhoneNumberUrl,
-                    WebsiteUrl = item.WebsiteUrl,
-                    MapUrl = item.MapUrl,
-                    WebsiteName = item.WebsiteName
-                });
+                    Locations.Add(new Location
+                    {
+                        Title = item.Title,
+                        Description = item.Description,
+                        HotelName = item.HotelName,
+                        HotelStandardsTitle = item.HotelStandardsTitle,
+                        HotelStandards = item.HotelStandards,
+                        Address = item.Address,
+                        PhoneNumber = item.PhoneNumber,
+                        DirectionsFromAirport = item.DirectionsFromAirport,
+                        ParkingInformation = item.ParkingInformation,
+                        LocalAttractionsTitle = item.LocalAttractionsTitle,
+                        LocalAttractions = item.LocalAttractions,
+                        Image = ImageSource.FromStream(() => new MemoryStream(item.Image)),
+                        LearnMoreUrl = item.LearnMoreUrl,
+                        BookNowUrl = item.BookNowUrl,
+                        PhoneNumberUrl = item.PhoneNumberUrl,
+                        WebsiteUrl = item.WebsiteUrl,
+                        MapUrl = item.MapUrl,
+                        WebsiteName = item.WebsiteName
+                    });
+                }
+                Pages = Locations;
             }
-            Pages = Locations;
+            catch (Exception)
+            {
+                await ErrorRetryManager.HandleRetryAsync(this, parameters, attempt);
+            }
         }
     }
 }

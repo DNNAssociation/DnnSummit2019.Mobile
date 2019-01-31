e0b212a0a8528f6d09c508dc075a0ba5b882a5d7
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-01-31 13:13:35 -0500

    Added portrait lock for Android

--- src/DnnSummit.Android/MainActivity.cs
+++ src/DnnSummit.Android/MainActivity.cs
@@ -6,7 +6,12 @@ using ImageCircle.Forms.Plugin.Droid;
 
 namespace DnnSummit.Droid
 {
-    [Activity(Label = "Dnn Summit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Dnn Summit", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        ConfigurationChanges = ConfigChanges.ScreenSize,
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

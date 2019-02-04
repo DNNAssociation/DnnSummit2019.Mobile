1b7b654c361f1e3fa00e274ec675d7185560a400
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-02-04 18:09:52 -0500

    added service dependencies

--- src/DnnSummit/ViewModels/FeedbackViewModel.cs
+++ src/DnnSummit/ViewModels/FeedbackViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace DnnSummit.ViewModels
+﻿using DnnSummit.Data.Services.Interfaces;
+using Prism.Mvvm;
+
+namespace DnnSummit.ViewModels
 {
-    public class FeedbackViewModel
+    public class FeedbackViewModel : BindableBase
     {
         public string Title => "Feedback";
+        protected IFeedbackService FeedbackService { get; }
+        protected IFeedbackEndpointService FeedbackEndpointService { get; }
+
+        public FeedbackViewModel(
+            IFeedbackService feedbackService,
+            IFeedbackEndpointService feedbackEndpointService)
+        {
+            FeedbackService = feedbackService;
+            FeedbackEndpointService = feedbackEndpointService;
+        }
     }
 }
